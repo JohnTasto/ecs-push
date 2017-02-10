e28dcc6b406647d0de2b96b687dcbb330b2a9b3b
John Tasto <john@tasto.net>
2017-02-09 20:14:09 -0800

    ECS deploy test 2

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -5,7 +5,10 @@
 source ./bin/env.sh
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "docker login..."
   docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+  echo "docker push..."
   docker push "$DOCKER_REPO"/"$DOCKER_WEB_IMAGE":latest
+  echo "ecs-cli compose..."
   ecs-cli compose --project-name "$ECS_PROJECT" --file ecs-deploy.yml service up;
 fi
